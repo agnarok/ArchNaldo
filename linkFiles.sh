7513a3a3baeb9595770ce0793fd94495b0fb382a
Agnaldo Brito Junior <pretogildo@gmail.com>
2021-06-24 13:56:58 -0400

    forcing symbolic links

--- linkFiles.sh
+++ linkFiles.sh
@@ -1,3 +1,3 @@
-ln -r --symbolic .gitconfig ~/.gitconfig
-ln -r --symbolic .condarc ~/.condarc
-ln -r --symbolic .nanorc ~/.nanorc
+ln -r --symbolic --force .gitconfig ~/.gitconfig
+ln -r --symbolic --force .condarc ~/.condarc
+ln -r --symbolic --force .nanorc ~/.nanorc
